ab2a1b95da291e93666c88621c7d87fbdebfc2ba
Ubuntu <ubuntu@ip-172-31-25-132.us-west-2.compute.internal>
2017-02-23 03:21:07 +0000

    test

--- loginandregistration/settings.py
+++ loginandregistration/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vq6cc0u!h9xamy4$!_y!x+h_qiey4g9!_bqep@)*0728fql(h8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.40.37.117']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
